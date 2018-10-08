763d3343bf9c48ac41fd9e7d0a62177f4032f0d2
SaraQian <abigail830@163.com>
2018-10-08 22:07:38 +0800

    first commit with springboot 2.0.5

---
+++ src/main/java/com/github/abigail830/wishlist/WishListApplication.java
@@ -0,0 +1,12 @@
+package com.github.abigail830.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/abigail830/wishlist/WishListApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.abigail830.wishlist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WishListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
