09055251fc31adf7b4b82ce3f9dcb02a837f7292
SaraQian <abigail830@163.com>
2018-10-11 13:07:55 +0800

    Adding swagger support

--- src/main/java/com/github/abigail830/wishlist/controller/WxController.java
+++ src/main/java/com/github/abigail830/wishlist/controller/WxController.java
@@ -2,6 +2,9 @@ package com.github.abigail830.wishlist.controller;
 
 import com.github.abigail830.wishlist.util.HttpClientUtil;
 import com.github.abigail830.wishlist.util.WXBizDataCrypt;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +27,10 @@ public class WxController {
 	@Value("${app.appSecret}")
 	private String appSecret;
 
+	@ApiOperation(value = "Handle wechat login",
+			notes = "小程序用户登陆处理",
+			response = String.class)
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功")})
 	@GetMapping(value = "/wxLogin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public String login(HttpServletRequest request) {
@@ -35,6 +42,10 @@ public class WxController {
 						"&js_code=" + code);
 	}
 
+	@ApiOperation(value = "Handle wechat info decryption",
+			notes = "小程序用户消息解密",
+			response = String.class)
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "请求成功")})
 	@GetMapping(value = "/decrypt", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public String decrypt(HttpServletRequest request) {
---
+++ src/main/java/com/github/abigail830/wishlist/util/swagger/ResourceConfig.java
@@ -0,0 +1,17 @@
+package com.github.abigail830.wishlist.util.swagger;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class ResourceConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
