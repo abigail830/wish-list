442f738d4cf71f284d827c547dc8d8623eedf904
SaraQian <abigail830@163.com>
2018-10-11 14:10:36 +0800

    Adding SQL for create table

---
+++ src/main/resources/sql/create_tables.sql
@@ -0,0 +1,40 @@
+USE wishlist;
+
+CREATE TABLE user_tbl (
+  open_id varchar(255) NOT NULL,
+  union_id varchar(255) NOT NULL,
+  gender varchar(45) DEFAULT NULL,
+  nickName varchar(255) DEFAULT NULL,
+  city varchar(255) DEFAULT NULL,
+  country varchar(255) DEFAULT NULL,
+  province varchar(255) DEFAULT NULL,
+  lang varchar(45) DEFAULT NULL,
+  PRIMARY KEY (open_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE wishlist_tbl (
+  ID int NOT NULL AUTO_INCREMENT,
+  open_id varchar(255) NOT NULL,
+  description varchar(255) DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  wishes varchar(255) DEFAULT NULL,
+  PRIMARY KEY (ID,open_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE wish_tbl (
+  ID int NOT NULL AUTO_INCREMENT,
+  wishlist_id int NOT NULL,
+  description varchar(255) DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_DATE(),
+  last_update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status varchar(255) DEFAULT NULL,
+  implementor_open_id varchar(255) DEFAULT NULL,
+  PRIMARY KEY (ID,wishlist_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE user_event (
+  open_id varchar(255) NOT NULL,
+  event_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  event_type varchar(255) NOT NULL,
+  PRIMARY KEY (open_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
