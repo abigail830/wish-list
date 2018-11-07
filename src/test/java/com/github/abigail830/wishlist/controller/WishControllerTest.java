49b56b4ffa17e439dd3ebeec0176bc28b6d13016
SaraQian <abigail830@163.com>
2018-11-07 16:01:11 +0800

    Adding baseline for flyway to avoid exception in jenkins

--- src/test/java/com/github/abigail830/wishlist/controller/WishControllerTest.java
+++ src/test/java/com/github/abigail830/wishlist/controller/WishControllerTest.java
@@ -34,7 +34,6 @@ public class WishControllerTest {
         ds = new JdbcDataSource();
         ds.setURL("jdbc:h2:mem:WishControllerTest;DB_CLOSE_DELAY=-1;MODE=MYSQL");
         Flyway flyway = Flyway.configure().dataSource(ds).load();
-        flyway.baseline();
         flyway.migrate();
         jdbcTemplate = new JdbcTemplate(ds);
         Toggle.TEST_MODE.setStatus(true);
