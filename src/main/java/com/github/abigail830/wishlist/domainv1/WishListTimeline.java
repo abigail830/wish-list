54431c5f52f40280c696dafc9090530b94846600
bryan-cheung <bryan.l.cheung@gmail.com>
2018-12-05 21:46:40 -0500

    fix month parsing issue

--- src/main/java/com/github/abigail830/wishlist/domainv1/WishListTimeline.java
+++ src/main/java/com/github/abigail830/wishlist/domainv1/WishListTimeline.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class WishListTimeline {
-    @JsonProperty("wishListTimeLinesªªª")
     private List<WishListTimelineEntry> wishListTimelineEntryList;
 
 
