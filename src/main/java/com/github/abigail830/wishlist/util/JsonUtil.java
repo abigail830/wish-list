6e6d92b8873f0a38fac2ebc2b3b7ccfb801f8a0a
SaraQian <abigail830@163.com>
2019-02-11 17:43:25 +0800

    remove useless mybatis and deprecate function in JsonUtil

--- src/main/java/com/github/abigail830/wishlist/util/JsonUtil.java
+++ src/main/java/com/github/abigail830/wishlist/util/JsonUtil.java
@@ -2,13 +2,11 @@ package com.github.abigail830.wishlist.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 import java.lang.reflect.Type;
 
 public class JsonUtil {
 	private final static Gson DEFAULT_GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
-	private final static Gson PRETTY_PRINT_GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").setPrettyPrinting().create();
 
 	public static <T> T toObject(String json, Type obj) {
 		return DEFAULT_GSON.fromJson(json, obj);
@@ -22,11 +20,4 @@ public class JsonUtil {
 		return DEFAULT_GSON.toJson(obj);
 	}
 
-	public static String toPrettyPrintedJson(Object obj) {
-		return PRETTY_PRINT_GSON.toJson(obj);
-	}
-
-	public static <T> Type makeParameterizedType(Class<T> containerType, Type... subType) {
-		return ParameterizedTypeImpl.make(containerType, subType, null);
-	}
 }
